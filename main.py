ff326ac3d563ba2fdc5fcef9243589feb006767a
Adrian Jaworek <adrian@kolegaliterat.pl>
2021-03-11 22:17:51 +0100

    Save data method will now iteraote over lists with mecha scales

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-from modules.mirageShopParser import MirageShopParser
-from modules.zincMechaParser import ZincMechaParser
+from modules.local_data_creator import LocalDataCraeator
 
 
 def main():
-    zinc_mecha_mg = ZincMechaParser('mg-1-100')
-    # zinc_mecha_hg = ZincMechaParser('rg-1-144')
-    mh_shop = MirageShopParser('Perfect-Grade-PG-160')
+    local = LocalDataCraeator('data/products.json')
 
-    try:
-        zinc_mecha_products_mg = zinc_mecha_mg.parse_products()
-    except Exception as parser_error:
-        print(parser_error)
-    else:
-        for element in zinc_mecha_products_mg:
-            print(element)
+    local.save_data()
 
 
-    # zinc_mecha_products_hg = zinc_mecha_hg.parse_products()
-
-    # test = mh_shop.parse_products()
-    #
-    # dataframe = pd.DataFrame(test)
 if __name__ == '__main__':
     main()
--- modules/local_data_creator.py
+++ modules/local_data_creator.py
@@ -10,4 +10,31 @@ class LocalDataCraeator():
         self.data_path = data_path
 
     def save_data(self):
-        pass
+        zinch_mecha_scales: list[str] = ["mg-1-00", 'hg-1-144', 'pg-1-60']
+        mirage_shop_scales: list[str] = ["High-Grade-HG-1144", 'Real-Grade-RG-1144', 'Master-Grade-MG-1100',
+                                         'Perfect-Grade-PG-160']
+
+        try:
+            for zinch_mecha_scale in zinch_mecha_scales:
+                data = self.__get_zipped_data(zinch_mecha_scale, 'zinch')
+
+                if data is not None:
+                    for element in data:
+                        print(element)
+        except Exception as data_error:
+            print(data_error)
+
+    def __get_zipped_data(self, scale: str, shop: str):
+        zipped_data = None
+
+        try:
+            if shop == 'zinch':
+                catalogue = ZincMechaParser(scale)
+                zipped_data = catalogue.parse_products()
+            elif shop == 'mirage':
+                catalogue = MirageShopParser(scale)
+                zipped_data = catalogue.parse_products()
+        except Exception as data_error:
+            print(data_error)
+        else:
+            return zipped_data
