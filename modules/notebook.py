6342867371feda0708643b83f2a516e037f0dd27
Adrian Jaworek <adrian@kolegaliterat.pl>
2021-03-20 17:57:35 +0100

    Added date check for notebook

--- modules/notebook.py
+++ modules/notebook.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime as dt
 import pandas as pd
 from modules.local_data_creator import LocalDataCraeator
 
@@ -9,18 +9,28 @@ def __get_data(filename: str):
     local = LocalDataCraeator(filename, False)
 
     try:
-        __get_last_save_date()
+        __is_update_needed()
 
     except Exception:
         print('No file!')
         # local.save_data()
     else:
-        print('File exist!')
+        if __is_update_needed():
+            local.save_data()
+        else:
+            dataframe = pd.read_csv(filename)
+            display(dataframe)
 
-def __get_last_save_date():
-    last_save = "EMPTY"
+def __is_update_needed() -> bool:
+    now = dt.datetime.now()
+    is_date_different = True
 
     with open('data/data_save.txt') as date_file:
         date: str = date_file.readline()
 
-        print(date)
+        last_save = dt.datetime.strptime(date, '%d-%m-%Y')
+
+        if last_save.date() == now.date():
+            is_date_different = False
+
+    return is_date_different
