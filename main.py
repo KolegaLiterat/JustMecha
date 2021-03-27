703a89647358b7b6bdb53460a5d9f979bcd47504
Adrian Jaworek <adrian@kolegaliterat.pl>
2021-03-27 17:53:35 +0100

    Added dataframe manager

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import pandas as pd
 import os
 import pprint as pprint
 import click
-from modules.localDataCreator import LocalDataCraeator
+from modules.dataframeManager import DataframeManager
 
 
 @click.command()
 @click.option('--mecha_name', help='Write a name of mecha model, that you\'re looking for')
 @click.argument('mecha_name')
-@click.option('--mecha_scale', default='ALL', help='Specify a mecha scale (ALL, HG, RG, MG, PG)')
-@click.argument('mecha_scale')
-def main(mecha_name, mecha_scale):
-    pprint.pprint(f'{mecha_name} {mecha_scale}')
+# @click.option('--mecha_scale', default='ALL', help='Specify a mecha scale (ALL, HG, RG, MG, PG)')
+# @click.argument('mecha_scale')
+def main(mecha_name):
+    df_manager = DataframeManager()
+
+    pprint.pprint(f'{mecha_name}')
 
 
 if __name__ == '__main__':
---
+++ modules/dataframeManager.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import datetime as dt
+from dataclasses import dataclass
+from modules.localDataCreator import LocalDataCraeator
+
+
+@dataclass()
+class DataframeManager(LocalDataCraeator):
+
+    def get_data(self):
+        try:
+            self.__is_update_needed()
+
+        except Exception:
+            dataframe = self.__create_dataframe()
+        else:
+            if self.__is_update_needed():
+                self.save_data()
+            else:
+                dataframe = self.__create_dataframe()
+
+        return dataframe
+
+
+    def __is_update_needed(self) -> bool:
+        now = dt.datetime.now()
+        is_date_different = True
+
+        with open('data/data_save.txt') as date_file:
+            date: str = date_file.readline()
+
+            last_save = dt.datetime.strptime(date, '%d-%m-%Y')
+
+            if last_save.date() == now.date():
+                is_date_different = False
+
+        return is_date_different
+
+
+    def __create_dataframe(self) -> pd.DataFrame:
+        header = ['Mecha', 'Price', 'Seen', 'Scale', 'Shop']
+
+        try:
+            dataframe = pd.read_csv(self.data_path, names=header)
+        except Exception:
+            local.save_data()
+            dataframe = pd.read_csv(self.data_path, names=header)
+
+        return dataframe
